0a1d31f78c70040d83c73e84bb7a1e3c82df4a0c
JeremiahShafer <88599453+Take-A-Breath@users.noreply.github.com>
2022-09-28 16:31:42 -0500

    While loops, flag, break, and continue

---
+++ using_flags.py
@@ -0,0 +1,13 @@
+# This flag, which we’ll call active (though you can call it anything),
+# will monitor whether or not the program should continue running:
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+    
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
